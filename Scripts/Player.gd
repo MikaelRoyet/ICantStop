8ce145f1faa6ffc05bce0dc6b4e65c75e28b810b
Michel <mikael.royet@gmail.com>
2022-07-25 17:17:40 +0200

    Initial Commit files

---
+++ Scripts/Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const SPEED = 100
+var velocityPlayer = Vector2(0,0)
+var idle = true
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	velocityPlayer = move_and_slide(velocityPlayer)
+	
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision:
+			if collision.collider is TileMap:
+				print("DEADGE")
+
+
+func _on_SwipeDetector_swiped(direction):
+	
+	#UP
+	if direction.x == 0 and direction.y == -1:
+		velocityPlayer = Vector2(0, SPEED)
+	
+	#DOWN
+	if direction.x == 0 and direction.y == 1:
+		velocityPlayer = Vector2(0, -SPEED)
+		
+	#LEFT
+	if direction.x == -1 and direction.y == 0:
+		velocityPlayer = Vector2(SPEED, 0)
+		
+	#RIGHT
+	if direction.x == 1 and direction.y == 0:
+		velocityPlayer = Vector2(-SPEED, 0)
+	
+func death():
+	pass
+	
+
---
+++ Scripts/SwipeDetector.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+signal swiped(direction)
+signal swiped_canceled(start_position)
+
+export(float, 1.0, 1.5) var MAX_DIAGONAL_SLOPE = 1.3
+
+onready var timer = $Timer
+var swipe_start_position = Vector2()
+
+func _input(event):
+	if not event is InputEventScreenTouch:
+		return
+	if event.pressed:
+		_start_detection(event.position)
+	elif not timer.is_stopped():
+		_end_detection(event.position)
+
+func _start_detection(position):
+	swipe_start_position = position
+	timer.start()
+
+func _end_detection(position):
+	timer.stop()
+	var direction = (position - swipe_start_position).normalized()
+	if abs(direction.x) + abs(direction.y) >= MAX_DIAGONAL_SLOPE:
+		return
+	if abs(direction.x) > abs(direction.y):
+		emit_signal('swiped', Vector2(-sign(direction.x), 0.0))
+	else:
+		emit_signal('swiped', Vector2(0.0, -sign(direction.y)))
+
+func _ready():
+	pass # Replace with function body.
+	
+
+func _on_Timer_timeout():
+	emit_signal('swiped_canceled', swipe_start_position)
