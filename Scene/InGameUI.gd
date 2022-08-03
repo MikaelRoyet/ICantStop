e136dbc45dc69debc7fb5a2aa7cf07aa4d185d29
Michel <mikael.royet@gmail.com>
2022-08-03 20:27:48 +0200

    In game UI v0.2

---
+++ Scene/InGameUI.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene(GameManager.MENU_SCENE)
+
+
+func _on_SoundOffButton_pressed():
+	pass # Replace with function body.
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,4 +2,5 @@ extends Node
 
 const SPEED = 150
 const SPEED_MODIFIER_ACCELERATION = 2
+const MENU_SCENE = "res://Scene/MainMenu.tscn"
 
