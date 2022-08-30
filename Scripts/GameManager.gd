96f2651018733a4b0817f94a53e7be1c646c529a
Aerkaos <mikael.royet@gmail.com>
2022-08-31 00:16:23 +0200

    Level 05

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const SPEED = 250
+const SPEED = 300
 const SPEED_MODIFIER_ACCELERATION = 2
 const MENU_SCENE = "res://Scenes/UI/MainMenu.tscn"
 const LEVEL_DATA = "res://Data/data_levels.json"
@@ -48,7 +48,9 @@ func load_level_data():
 func goToNextLevel():
 	print(presentLevel)
 	get_tree().change_scene("res://Scenes/Levels/" + levelDataDict[presentLevel.split('.')[0]]["nextLevel"] + ".tscn")
+	presentLevel = levelDataDict[presentLevel.split('.')[0]]["nextLevel"]
 
+	
 	
 func resetLevel():
 	get_tree().reload_current_scene()
