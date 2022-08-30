95820142c805d19bf4c2543974f3838112d1f477
Aerkaos <mikael.royet@gmail.com>
2022-08-30 19:12:34 +0200

    Reforge Level 3

--- Scripts/Objects/EndLevel.gd
+++ Scripts/Objects/EndLevel.gd
@@ -1,16 +1,5 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func _on_EndLevel_body_entered(body):
 	if body.is_in_group("Player"):
 		GameManager.goToNextLevel()
