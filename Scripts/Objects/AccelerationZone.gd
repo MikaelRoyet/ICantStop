f456b72b4e8fbe819ee8c1a3937e8c126b4922d4
Aerkaos <mikael.royet@gmail.com>
2022-09-06 17:45:32 +0200

    Modification speed tile
    now speed tile only boost when on it

--- Scripts/Objects/AccelerationZone.gd
+++ Scripts/Objects/AccelerationZone.gd
@@ -7,3 +7,8 @@ var speedModifier = GameManager.SPEED_MODIFIER_ACCELERATION
 func _on_AccelerationZone_body_entered(body):
 	if "Player" in body.name:
 		body.modifySpeedModifier(speedModifier)
+
+
+func _on_AccelerationZone_body_exited(body):
+	if "Player" in body.name:
+		body.modifySpeedModifier(1)
