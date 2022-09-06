ada94fa586b27685d2fa36bdad913b47e7f01b3c
Aerkaos <mikael.royet@gmail.com>
2022-09-06 17:36:10 +0200

    Ajout block bumper death
    un bloc bumper mais qui elimine le joueur si il le touche

---
+++ Scripts/Objects/deathBlock.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+
+const SPEED = GameManager.SPEED
+var velocityBumper = Vector2(0,0)
+var lastMovement
+
+
+
+func _ready():
+	lastMovement = Vector2(0,0)
+
+func _physics_process(delta):
+	velocityBumper = move_and_slide(velocityBumper)
+	
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision:
+			if collision.collider.is_in_group("Bumper"):
+				print("collisionge")
+				if collision.collider.has_method("move"):
+					print("collisionge2")
+					collision.collider.move(lastMovement)
+					bounce()
+
+
+
+func move(movement):
+	velocityBumper = movement
+	lastMovement = movement
+
+
+func bounce():
+	move(lastMovement * -1)
+
+func moveToPoint(point):
+	position = point
