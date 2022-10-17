86928efab5a7154bef1700b6741c3d397d0a8d2a
Michel <mikael.royet@gmail.com>
2022-10-18 00:36:35 +0200

    Animation acceleration zone
    J'ai eu un soucis avec le commit flemme de refaire le message

--- Scripts/Objects/AccelerationZone.gd
+++ Scripts/Objects/AccelerationZone.gd
@@ -3,6 +3,10 @@ extends Area2D
 
 var speedModifier = GameManager.SPEED_MODIFIER_ACCELERATION
 
+func _ready():
+	var anim = get_node("AnimationPlayer").get_animation("colors")
+	anim.set_loop(true)
+	get_node("AnimationPlayer").play("colors")
 
 func _on_AccelerationZone_body_entered(body):
 	if "Player" in body.name:
